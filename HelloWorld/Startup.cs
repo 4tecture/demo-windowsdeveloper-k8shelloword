13771546bf19311fc3dc5d3fd53e75c25f887dd6
Marc Müller <marc.mueller@4tecture.ch>
2020-09-21 12:30:18 +0200

    initial commit

---
+++ HelloWorld/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using HelloWorld.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddSingleton(new ConfigurationOptions
+            {
+                Message = Configuration["Message"],
+                DeploymentEnvironment = Configuration["DeploymentEnvironment"]
+            });
+
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapHealthChecks("/health");
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# Docker Hello World
+This solution and docker container is intended to be used as an introcutionary sample to docker. You learn how to build a docker container and run it. Environment variables provide the ability to change the text which is displayed on the home page.
+
+# Building the container
+- Build the image based on dockerfile
+  - `docker build -t <reponame>:<tag> .`
+  - `docker build -t 4tecture/hello-world:v2.1 -t 4tecture/hello-world:latest -f .\HelloWorld\Dockerfile .`
+- Verify image
+  - `docker images 4tecture/hello-world`
+- Publish the image
+  - `docker login`
+  - `docker push 4tecture/hello-world:v2.1`
+  - `docker push 4tecture/hello-world:latest`
+
+# Running the demo
+- Visit [https://hub.docker.com](https://hub.docker.com) and search for 4tecture
+- Choose `hello-world`
+- `docker images`
+- `docker run -d -p 8080:80 --name hello-world 4tecture/hello-world`
+- `docker images`
+- `docker ps`
+- Show app in browser, show hostname under about
+- `docker stop hello-world` 
+- `docker ps`
+- `docker ps -a`
+- `docker container rm hello-world`
+- `docker run -d -p 8080:80 -e Message='Hello dear audience member!' --name hello-world 4tecture/hello-world`
+
+# Showing the docker file system
+- `docker ps`
+- `docker exec -i -t hello-world /bin/bash`
+- `pwd`
+- `ls -l`
+- `exit`
+
+# Cleanup for demos
+- `docker rm -f $(docker ps -a -q)`
+- `docker rmi -f $(docker images -q)`
